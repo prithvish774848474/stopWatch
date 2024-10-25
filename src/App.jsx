52c0c0d78e6ab906ceebee4afc1cc26104625d80
prithvish774848474 <prithvish.freelance@gmail.com>
2024-10-25 17:04:27 +0530

    refactor: Using 'tailwind.css' file for styling where I modified the UI and refactored the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from "react";
-import './App.css';
+import './tailwind.css';
 
 export default function App() {
   const [isActive, setIsActive] = useState(false);
@@ -18,33 +18,87 @@ export default function App() {
   };
 
   return (
-    <div className="stopWatchContainer">
-      <span className="stopWatch">
-        {Math.floor(((time / 60) / 60) % 60).toString().padStart(2, '0')}
-        <kbd className="timeDesignation">h</kbd> : {" "}
-        {Math.floor((time / 60) % 60).toString().padStart(2, "0")}
-        <kbd className="timeDesignation">m</kbd> :{" "}
-        {Math.floor(time % 60).toString().padStart(2, "0")}
-        <kbd className="timeDesignation">s</kbd>
+    <div className="bg-transparent
+    border-none
+    rounded-2xl
+    w-[75vw] md:w-[50vw]
+    h-[90vh]
+    flex flex-col
+    justify-around
+    items-center
+    custom-container-box-shadow">
+      <span className="w-[120%]
+      h-[30%] md:h-[40%]
+      text-4xl md:text-6xl lg:text-7xl
+      text-sky-700
+      font-sans
+      bg-transparent
+      border-none
+      rounded-xl md:rounded-2xl
+      flex flex-row
+      justify-center
+      items-center
+      custom-stopwatch-box-shadow">
+        {Math.floor((time / 60 / 60) % 60)
+          .toString()
+          .padStart(2, "0")}
+        <span className="text-2xl">h</span> : &nbsp;
+        {Math.floor((time / 60) % 60)
+          .toString()
+          .padStart(2, "0")}
+        <span className="text-2xl">m</span> : &nbsp;
+        {Math.floor(time % 60)
+          .toString()
+          .padStart(2, "0")}
+        <span className="text-2xl">s</span>
         {/* time % 60 gives seconds
         (time / 60) % 60 gives minutes
         ((time / 60) / 60) % 60 gives hours
         number % 60 is in the range [0, 59] */}
       </span>
-      <button
-        type="button"
-        className="controlButton start-stop"
-        onClick={() => setIsActive(!isActive)}
-      >
-        {isActive ? "Stop" : "Start"}
-      </button>
-      <button
-        type="button"
-        className="controlButton reset"
-        onClick={handleReset}
-      >
-        Reset
-      </button>
+      <section className="flex flex-col md:flex-row
+      justify-around
+      items-center
+      gap-4
+      w-full
+      h-[30%]">
+        <button
+          type="button"
+          className="p-4 md:p-6
+          text-2xl
+          text-center
+          text-amber-400
+          font-mono
+          font-bold
+          italic
+          bg-transparent
+          rounded-[50%]
+          border-2 border-amber-200 border-b-transparent
+          hover:border-transparent hover:border-b-amber-200
+          cursor-pointer custom-control-button-transition"
+          onClick={() => setIsActive(!isActive)}
+        >
+          {isActive ? "Stop" : "Start"}
+        </button>
+        <button
+          type="button"
+          className="p-4 md:p-6
+          text-2xl
+          text-center
+          text-rose-400
+          font-mono
+          font-bold
+          italic
+          bg-transparent
+          rounded-[50%]
+          border-2 border-rose-200 border-b-transparent
+          hover:border-transparent hover:border-b-rose-200
+          cursor-pointer custom-control-button-transition"
+          onClick={handleReset}
+        >
+          Reset
+        </button>
+      </section>
     </div>
   );
 }
